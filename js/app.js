c4119cd146d3c74e790501cff6f6f4b85af35d9f
Lucas <lucasrukhan@gmail.com>
2022-03-14 20:22:04 -0300

    att menu 2.1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu() {
+    if(event.type === 'touchstart') event.preventDefault();
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
